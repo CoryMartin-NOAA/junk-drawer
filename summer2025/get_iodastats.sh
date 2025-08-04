5d69a907590e97187c0c8c417ac56a710f3a0f52
Cory Martin <Cory.R.Martin@noaa.gov>
2025-08-04 13:30:59 +0000

    add get_iodastats

---
+++ summer2025/get_iodastats.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Script to get IODA stats from the HPSS
+# Usage: get_iodastats.sh <start_date> <end_date> <hpss_root> <output_directory>
+# start_date and end_date should be in YYYYMMDDHH format
+
+START=${START:-$1}
+END=${END:-$2}
+HPSS_ROOT=${HPSS_ROOT:-$3}
+OUTDIR=${OUTDIR:-$4}
+
+if [ -z "$START" ] || [ -z "$END" ] || [ -z "$HPSS_ROOT" ] || [ -z "$OUTDIR" ]; then
+    echo "Usage: $0 <start_date> <end_date> <hpss_root> <output_directory>"
+    exit 1
+fi
+
+# Create output directory if it does not exist
+mkdir -p "$OUTDIR"
+
+# Loop through the date range
+current_date="$START"
+cd "$OUTDIR" || exit 1
+while [ "$current_date" -le "$END" ]; do
+    # Construct the HPSS path
+    PDY=$(echo "$current_date" | cut -c1-8)  # YYYYMMDD
+    cyc=$(echo "$current_date" | cut -c9-10)  # HH
+    hpss_path="$HPSS_ROOT/$current_date/gdas.tar"
+    # gdas.20250217/00/products/snow/anlmon/gdas.t00z.snow_iodastat.tgz
+    hpss_file="gdas.${PDY}/${cyc}/products/snow/anlmon/gdas.t${cyc}z.snow_iodastat.tgz"
+    htar -xvf $hpss_path $hpss_file
+    if [ $? -ne 0 ]; then
+        echo "Failed to extract $hpss_file from $hpss_path"
+    else
+        echo "Extracted $hpss_file to $OUTDIR"
+    fi
+    # extract the stat files
+    cd "$OUTDIR/gdas.${PDY}/${cyc}/products/snow/anlmon" || exit 1
+    tar -xzf "gdas.t${cyc}z.snow_iodastat.tgz"
+    if [ $? -ne 0 ]; then
+        echo "Failed to extract gdas.t${cyc}z.snow_iodastat.tgz"
+    else
+        echo "Extracted gdas.t${cyc}z.snow_iodastat.tgz to $OUTDIR/gdas.${PDY}/${cyc}/products/snow/anlmon"
+    fi
+    # Move back to the output directory
+    cd "$OUTDIR" || exit 1
+    # Remove the tar file to save space
+    rm -f "$hpss_file"
+    # Increment the date by 6 hours
+    current_date=$(date -d "${current_date:0:8} ${current_date:8:2} +6 hours" +%Y%m%d%H)
+done
